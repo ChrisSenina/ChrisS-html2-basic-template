350862f839a343578586c7a6f299c12a54a3ee98
Christina Senina <christina.senina@mail.ru>
2024-02-22 22:44:21 +0300

    Стилизует слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,9 +2,41 @@
 const nav = document.querySelector('.nav');
 const menuButton = document.querySelector('.nav__toggle');
 
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.slider__button-prev');
+const nextButton = document.querySelector('.slider__button-next');
+const slides = Array.from(slider.querySelectorAll('.slider__item'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
 nav.classList.add('nav--closed');
 
 menuButton.addEventListener('click', () => {
   nav.classList.toggle('nav--closed');
   nav.classList.toggle('nav--opened');
 });
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+updateSlider()
