a3263cb64aa9f86087525ffeaf0c0ca41660ce23
Christina Senina <christina.senina@mail.ru>
2024-02-22 15:38:49 +0300

    Добавляет проект

---
+++ source/scripts/index.js
@@ -0,0 +1,10 @@
+/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const menuButton = document.querySelector('.nav__toggle');
+
+nav.classList.add('nav--closed');
+
+menuButton.addEventListener('click', () => {
+  nav.classList.toggle('nav--closed');
+  nav.classList.toggle('nav--opened');
+});
